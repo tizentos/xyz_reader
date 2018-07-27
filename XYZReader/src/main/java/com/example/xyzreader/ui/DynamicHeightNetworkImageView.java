5707dc1cb9cd8b5fe64889a77702ca63688f976e
tizentos <gorillar94>
2018-07-27 21:36:08 +0100

    Initial commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
@@ -0,0 +1,36 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.android.volley.toolbox.NetworkImageView;
+
+public class DynamicHeightNetworkImageView extends NetworkImageView {
+    private float mAspectRatio = 1.5f;
+
+    public DynamicHeightNetworkImageView(Context context) {
+        super(context);
+        this.setScaleType(ScaleType.FIT_XY);
+    }
+
+    public DynamicHeightNetworkImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.setScaleType(ScaleType.FIT_XY);
+    }
+
+    public DynamicHeightNetworkImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setAspectRatio(float aspectRatio) {
+        mAspectRatio = aspectRatio;
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measuredWidth = getMeasuredWidth();
+        setMeasuredDimension(measuredWidth, (int) (measuredWidth / mAspectRatio));
+    }
+}
